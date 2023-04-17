38048593b3d910e0456d98a66908a06d258ed8c8
lucascmpus <lucascmpus@gmail.com>
2023-04-17 19:26:57 -0300

    feat: add and style toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import router from './routes';
-import './i18n/index'
+import './i18n/index';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Toaster
+      toastOptions={{
+        success: {
+          style: {
+            background: '#2f72a2',
+            color: '#fff',
+          },
+        },
+      }}
+    />
   </React.StrictMode>,
 );
