e8fdc9fe8d4182055d08f1dd47f4d468ac6528d8
lucascmpus <lucascmpus@gmail.com>
2023-04-17 14:28:52 -0300

    feat: animation button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,14 +1,20 @@
 import { IoIosArrowBack } from 'react-icons/io';
 import { Link } from 'react-router-dom';
+import { motion as m } from 'framer-motion';
 
 function BackButton() {
   return (
     <>
-      <button className='absolute top-20 left-20 text-white'>
-        <Link to='/'>
+      <m.button
+        initial={{ x: 0 }}
+        animate={{ x: 100 }}
+        transition={{ duration: 1 }}
+        className="absolute top-20 left-20 text-white"
+      >
+        <Link to="/">
           <IoIosArrowBack />
         </Link>
-      </button>
+      </m.button>
     </>
   );
 }
