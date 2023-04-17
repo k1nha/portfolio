b01fbf5980da4c8419b564b690f7f4418aa62637
lucascmpus <lucascmpus@gmail.com>
2023-04-17 16:37:29 -0300

    feat: finish about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,6 +3,7 @@ import Background from '../components/Background';
 import { useTranslation } from 'react-i18next';
 import { HiOutlineLanguage } from 'react-icons/hi2';
 import BackButton from '../components/BackButton';
+import { motion as m } from 'framer-motion';
 
 const underlineEffect =
   "relative before:content-[''] before:absolute before:block  before:h-[2px] before:bottom-0 before:left-0 before:bg-white before:hover:scale-x-100 before:scale-x-0 before:origin-top-left before:transition before:ease-in-out before:duration-300";
@@ -24,67 +25,86 @@ function About() {
   return (
     <>
       <BackButton />
-      <button
-        className="absolute top-20 right-20 text-white bg-BabyBlue px-2 py-1 shadow-2xl rounded-md"
+
+      <m.button
+        initial={{ x: 100 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1 }}
+        className="absolute top-20 right-5 md:right-10 lg:right-20 text-white bg-BabyBlue px-2 py-1 shadow-2xl rounded-md"
         type="button"
         onClick={() => handleChangeLanguage()}
       >
         <HiOutlineLanguage />
-      </button>
+      </m.button>
 
-      <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center font-Montserrat text-white">
-        {/* ABOUT */}
-        <div className="flex flex-col gap-4 flex-wrap max-w-[750px] ">
-          <h1 className="text-3xl">{t('apresentacao-1')}</h1>
-          <p>{t('apresentacao-2')}</p>
+      <m.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center font-Montserrat text-white p-5 md:p-0">
+          {/* ABOUT */}
+          <div className="flex flex-col gap-4 flex-wrap max-w-[750px] ">
+            <div className="text-2xl font-bold flex justify-between">
+              <h1>{t('sobre')}</h1>
 
-          <p>{t('apresentacao-3')}</p>
+              <m.button
+                whileHover={{
+                  x: -20,
+                  transition: { duration: 0.3 },
+                }}
+                className="text-xl text-right text-transparent bg-clip-text bg-gradient-to-r from-[#4cbeff99] to-[#b7e5a2b0]"
+              >
+                <a href="../assets/lucas_campos_curriculum.pdf">
+                  {t('curriculo')}
+                </a>
+              </m.button>
+            </div>
 
-          {/* OBJECTIVE */}
-          <div className="">
-            <h1
-              className={
-                underlineEffect + ' before:w-[100px] font-bold text-xl'
-              }
-            >
-              {t('objetivo')}
-            </h1>
-            <p>{t('objetivos-1')}</p>
-            <p>{t('objetivos-2')}</p>
-          </div>
+            <h1 className="text-3xl text-BabyGreen">{t('apresentacao-1')}</h1>
+            <p>{t('apresentacao-2')}</p>
 
-          {/* EXPERIENCES */}
-          <div className="">
-            <h1
-              className={
-                underlineEffect + ' before:w-[128px] font-bold text-xl'
-              }
-            >
-              {t('experiencias')}
-            </h1>
+            <p>{t('apresentacao-3')}</p>
 
-            {/* INTERA */}
+            {/* OBJECTIVE */}
             <div className="">
-              <li className="text-lg">{t('intera')}</li>
-              <p>{t('intera-1')}</p>
+              <h1
+                className={
+                  underlineEffect + ' before:w-[100px] font-bold text-xl'
+                }
+              >
+                {t('objetivo')}
+              </h1>
+              <p>{t('objetivos-1')}</p>
+              <p>{t('objetivos-2')}</p>
             </div>
 
-            {/* FREELA */}
-            <div className="mt-4">
-              <li className="text-lg">{t('freelancer')}</li>
-              <p>{t('freelancer-1')}</p>
+            {/* EXPERIENCES */}
+            <div className="">
+              <h1
+                className={
+                  underlineEffect + ' before:w-[128px] font-bold text-xl'
+                }
+              >
+                {t('experiencias')}
+              </h1>
+
+              {/* INTERA */}
+              <div className="">
+                <li className="text-lg">{t('intera')}</li>
+                <p>{t('intera-1')}</p>
+              </div>
+
+              {/* FREELA */}
+              <div className="mt-4">
+                <li className="text-lg">{t('freelancer')}</li>
+                <p>{t('freelancer-1')}</p>
+              </div>
             </div>
-
-
           </div>
-          
         </div>
+      </m.div>
 
-        {/* OBJECTIVE */}
-        <div className="">
-          <p></p>
-        </div>
-      </div>
       <Background />
     </>
   );
